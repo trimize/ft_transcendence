423fa091e63541e3296b16fcdc31715c299abe0e
marcioflaviob <mbrandao@student.42.fr>
2024-10-12 11:25:36 +0200

    Implemented Match Record and improved directory organization for views

--- server/tcd_back/api/urls.py
+++ server/tcd_back/api/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
-from .views import get_users, create_user
+from .views import user_views, match_views
 
 urlpatterns = [
-	path('users/', get_users, name='get_users'),
-	path('add_user/', create_user, name='create_user'),
+	path('users/', user_views.get_users, name='get_users'),
+	path('create_user/', user_views.create_user, name='create_user'),
+	path('update_user/', user_views.update_user, name='update_user'),
+	path('delete_user/', user_views.delete_user, name='delete_user'),
+	path('users/<int:pk>/', user_views.get_user, name='get_user'),
+	path('search_user/<str:name>/', user_views.search_user, name='search_user')
+
+	path('matches/', match_views.get_matches, name='get_matches'),
+	path('create_match/', match_views.create_match, name='create_match'),
+	path('update_match/', match_views.update_match, name='update_match'),
+	path('delete_match/', match_views.delete_match, name='delete_match'),
+	path('matches/<int:pk>/', match_views.get_match, name='get_match'),
+	path('matches/player/<int:player_id>/', match_views.get_match_by_player, name='get_match_by_player')
 ]
