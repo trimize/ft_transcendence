6b240010e90884879d9ee785d0146ba69ae82959
marcioflaviob <mbrandao@student.42.fr>
2024-10-13 21:55:31 +0200

    Invitation system

--- server/tcd_back/api/consumers.py
+++ server/tcd_back/api/consumers.py
@@ -1,54 +1,82 @@
 import json
-import logging
 from channels.generic.websocket import AsyncWebsocketConsumer
 
-logger = logging.getLogger(__name__)
+user_channels = {}
 
 class SocketConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
-        await self.accept()
+	async def connect(self):
+		user_id = self.scope['user'].id
+        user_channels[user_id] = self.channel_name
+		await self.accept()
 
-    # async def disconnect(self, close_code):
-    #     # Leave room group
-    #     await self.channel_layer.group_discard(
-    #         self.room_group_name,
-    #         self.channel_name
-    #     )
+	async def disconnect(self, close_code):
+	    # Leave room group
+		user_id = self.scope['user'].id
+        if user_id in user_channels:
+            del user_channels[user_id]
 
-    async def receive(self, text_data):
-        try:
-            text_data_json = json.loads(text_data)
-        except json.JSONDecodeError as e:
-            logger.error(f"JSONDecodeError: {e}")
-            await self.close()
-            return
+	async def receive(self, text_data):
+		try:
+			text_data_json = json.loads(text_data)
+		except json.JSONDecodeError as e:
+			print(f"JSONDecodeError: {e}")
+			await self.close()
+			return
 
-        if 'type' in text_data_json:
-            if text_data_json['type'] == 'new_match':
-                match_id = text_data_json.get('matchId')
-                self.room_group_name = f'match_{match_id}'
-                logger.info(f"Adding to group: {self.room_group_name}")
-                await self.channel_layer.group_add(
-                    self.room_group_name,
-                    self.channel_name
-                )
-            elif text_data_json['type'] == 'match_update':
-                match_id = text_data_json.get('matchId')
-                self.room_group_name = f'match_{match_id}'
-                logger.info(f"Sending update to group: {self.room_group_name}")
-                await self.channel_layer.group_send(
-                    self.room_group_name,
-                    {
-                        'type': 'match_update_response',
-                        'message': text_data_json
-                    }
-                )
+		if 'type' in text_data_json:
+			if text_data_json['type'] == 'new_match':
+				match_id = text_data_json.get('matchId')
+				if not match_id:
+					print("Match ID not provided")
+					return
+				self.room_group_name = f'match_{match_id}'
+				print(f"Adding to group: {self.room_group_name}")
+				await self.channel_layer.group_add(
+					self.room_group_name,
+					self.channel_name
+				)
+			elif text_data_json['type'] == 'match_update':
+				match_id = text_data_json.get('matchId')
+				self.room_group_name = f'match_{match_id}'
+				print(f"Sending update to group: {self.room_group_name}")
+				await self.channel_layer.group_send(
+					self.room_group_name,
+					{
+						'type': 'match_update_response',
+						'message': text_data_json
+					}
+				)
+			elif text_data_json['type'] == 'send_invite':
+				invitee_id = text_data_json.get('inviteeId')
+				invitee_channel_name = user_channels.get(invitee_id)
+				match_id = text_data_json.get('matchId')
+				self.room_group_name = f'match_{match_id}'
+				await self.channel_layer.group_add(
+					self.room_group_name,
+					invitee_channel_name
+				)
+				# if invitee_id in user_channels:
+                #     invitee_channel_name = user_channels[invitee_id]
+                #     print(f"Sending invite to user {invitee_id} on channel {invitee_channel_name}")
+                #     await self.channel_layer.send(
+                #         invitee_channel_name,
+                #         {
+                #             'type': 'receive_invite',
+                #             'message': text_data_json
+                #         }
+                #     )
+                # else:
+                #     print(f"User {invitee_id} is not connected")
 
-    async def match_update_response(self, event):
+	async def match_update_response(self, event):
+		message = event['message']
+		print(f"Sending message: {message}")
+		await self.send(text_data=json.dumps(message))
+
+	async def receive_invite(self, event):
         message = event['message']
-        logger.info(f"Sending message: {message}")
+        print(f"Received invite: {message}")
         await self.send(text_data=json.dumps(message))
-
 
 	# async def chat_message(self, event):
 	#     message = event['message']
