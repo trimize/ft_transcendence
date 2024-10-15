5fc7af7b7e8abefb05b86b9a512fb1b468500e07
Alisa Tonkopiy <atonkopi@made-f0Br6s6.clusters.42paris.fr>
2024-10-15 14:16:45 +0200

    change in rending user profile

--- client/JS/Profile.js
+++ client/JS/Profile.js
@@ -1,18 +1,23 @@
 import { fetchUserData } from "./user_info.js";
 
-const usernameDiv = document.getElementById('username');
-const emailDiv = document.getElementById('email');
-const winsDiv = document.getElementById('wins');
-const lossesDiv = document.getElementById('losses');
+// const usernameDiv = document.getElementById('username');
+// const emailDiv = document.getElementById('email');
+// const winsDiv = document.getElementById('wins');
+// const lossesDiv = document.getElementById('losses');
+// const profilePicture = document.getElementById('profilePicture');
 
 document.addEventListener('DOMContentLoaded', function()
 {
-	fetchUserData().then(data =>
+	fetchUserData().then(profileData =>
 	{
-		usernameDiv.textContent = data.username;
-		emailDiv.textContent = data.email;
-		winsDiv.textContent = "Wins : " + data.wins;
-		lossesDiv.textContent = "Losses : " + data.losses;
+		const profilePicture = document.getElementById('profilePicture');
+        profilePicture.src = profileData.profile_picture || 'https://cdn-icons-png.flaticon.com/512/9203/9203764.png'; // Replace with your default URL if needed
+
+        // Update other profile information
+        document.getElementById('username').textContent = profileData.username;
+        document.getElementById('email').textContent = profileData.email;
+        document.getElementById('wins').textContent = `Wins : ${profileData.wins}`;
+        document.getElementById('losses').textContent = `Losses : ${profileData.losses}`;
 	})
 	.catch(error =>
 	{
