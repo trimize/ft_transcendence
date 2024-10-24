69a751e08922ccfb7a0a5c0f00ad58f5db3fea27
Alisa Tonkopiy <atonkopi@made-f0Br7s18.clusters.42paris.fr>
2024-10-24 14:38:45 +0200

    added new default profile pic

--- client/JS/appconfig.js
+++ client/JS/appconfig.js
@@ -1,2 +1,2 @@
-export const DEFAULT_PROFILE_PIC = 'https://cdn-icons-png.flaticon.com/512/9203/9203764.png';
+export const DEFAULT_PROFILE_PIC = '../Assets/default_user.jpg';
 export const BACKEND_URL = 'http://localhost:8000';
